844cec34f32ea2aac40d3c20fa90308063757a22
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2020-10-13 16:46:50 -0400

    index refactor

--- proximify/www/index.php
+++ proximify/www/index.php
@@ -1,11 +1,18 @@
 <?php
 
-// use composer autoload method
-require_once('./vendor/autoload.php');
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\App;
 
-$app = new App();
-$response = $app->handle();
+$params = [
+    'rootDir' => __DIR__ . '/..'
+];
 
-echo $response;
+if ($queryName = $_GET ? array_key_first($_GET) : false) {
+    $params['queryName'] = $queryName;
+} else {
+    $params['queryName'] = 'home';
+}
+
+$app = new App();
+echo $app->router($params);
