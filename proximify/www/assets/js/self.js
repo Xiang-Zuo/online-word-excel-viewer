b4117fb7ceddc6a4322f88299c9f1af8b48784f6
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2020-10-13 16:36:53 -0400

    css js refactor

--- proximify/www/assets/js/self.js
+++ proximify/www/assets/js/self.js
@@ -1,11 +1,11 @@
 $(document).ready(
-    $('.file-dropdown .dropdown-item').click(function() {
-        $.post('/file/get', { id: $(this).data('id') }, function(data) {
+    $('.file-dropdown .dropdown-item').click(function () {
+        $.post('/?file', {name: $(this).data('name')}, function (data) {
             let html = data.data;
             $('.file-content').html(html);
         }, 'json');
         $('.download-btn').text('Download:' + $(this).data('name'));
         $('.download-btn').removeClass('disabled');
-        $('.download-btn').attr('href',  $(this).data('path'));
+        $('.download-btn').attr('href', $(this).data('path'));
     })
 );
